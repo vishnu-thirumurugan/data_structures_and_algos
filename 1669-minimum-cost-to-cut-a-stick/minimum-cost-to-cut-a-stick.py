db0fe88f4eaf9b8e231bf7ba872a3899275ad974
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-11-23 09:03:31 +0530

    Time: 1206 ms (25.29%) | Memory: 21.8 MB (13.02%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -8,19 +8,23 @@ class Solution:
         # normalise the cuts array 
         cuts = [0] + sorted(cuts) + [n]
 
-        @lru_cache(None)
-        def dp(i,j): # min cost to cut stick starting at cuts[i] and ending at cuts[j]
+        memo = {}
+
+        def solve(i,j): # min cost to cut stick starting at cuts[i] and ending at cuts[j]
+            if (i,j) in memo:
+                return memo[(i,j)]
             if j - i == 1: # cannot cut between length of 1
                 return 0
-
             min_cost = float('inf')
             for k in range(i+1, j):
                 # Cost = Length of current stick + cost of left side + cost of right side
-                current_cost = (cuts[j] - cuts[i]) + dp(i,k) + dp(k,j)
+                current_cost = (cuts[j] - cuts[i]) + solve(i,k) + solve(k,j)
                 min_cost = min(min_cost, current_cost)
 
-            return min_cost
+            memo[(i,j)] = min_cost
+            return memo[(i,j)]
+            
         m = len(cuts)
         # solve for full stick
-        return dp(0, m-1)
+        return solve(0, m-1)
         
