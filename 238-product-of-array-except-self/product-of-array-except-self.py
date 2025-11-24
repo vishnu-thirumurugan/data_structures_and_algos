aa4dc43f75c244edac7d2292c3ddfe7538479fdb
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-11-24 07:20:57 +0530

    Time: 23 ms (64.12%) | Memory: 22.5 MB (99.93%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,16 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # solving using division is also tricky
         product = 1
         for i in nums:
             product = product * i
 
         zero_less_product = 1
-        for i in nums:
-            if i != 0:
-                zero_less_product = zero_less_product * i
-
-        if nums.count(0) > 1:
+        if nums.count(0) > 1: # if u have multiple zeros, its any way zero
             zero_less_product = 0
+        else:
+            for i in nums:
+                if i != 0:
+                    zero_less_product = zero_less_product * i
+
+        
 
         n = len(nums)
 
