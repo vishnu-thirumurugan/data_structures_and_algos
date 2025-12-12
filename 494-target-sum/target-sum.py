31afc7e5105cc5f553b160d7d0a8779ad09082f6
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-12 07:11:53 +0530

    Time: 199 ms (29.61%) | Memory: 25.6 MB (43.32%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,24 +1,44 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # convert this problem to knapsack --> gemini
+        # divide the array into two sets of positive and negative numbers
+        # p + n = total sum
+        # p-n = target
+        # 2p = total sum + target
+        # p = (total sum)+ target /2
         n = len(nums)
+        total_sum = sum(nums)
+        # edge cases --> early exit 
+        if (total_sum + target)% 2 != 0:
+            return 0
 
+        if target > total_sum:
+            return 0
+
+        p = (target+total_sum)//2 #--> this is your new target
+
+        # states --> (index, current_target)
+        # memo = {}
         @lru_cache(None)
-        def helper(idx, curr_sum):
+        def helper(index, curr_target):
             # base case
-            if idx == n:
-                if curr_sum == target:
-                    return 1
+            if index == n:
+                if curr_target == 0: return 1
                 return 0
 
-            output = (helper(idx + 1, curr_sum + nums[idx]) + 
-                        helper(idx + 1, curr_sum - nums[idx]))
+            ways = helper(index+1, curr_target)
+            print(index+1, curr_target)
+            if nums[index] <= curr_target:
+                ways += helper(index+1, curr_target-nums[index]) # leave or take
 
-            return output
+            return ways
 
-        return helper(0,0)
+        return helper(0,p)
+
 
 
             
+
 
             
 
