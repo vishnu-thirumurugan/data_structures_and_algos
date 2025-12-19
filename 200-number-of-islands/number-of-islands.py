7bfff1583c3dcd88dc5297a46582ae458026b362
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-19 16:13:03 +0530

    Time: 248 ms (53.05%) | Memory: 19.7 MB (99.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -8,22 +8,25 @@ class Solution:
 
         count = 0
 
-        def dfs(r,c):
-            visited[r][c] = 1
-            directions = [(0,1), (1,0), (0, -1), (-1,0)]
-
-            for dr, dc in directions:
-                nr, nc = r+dr, c+dc
-                if 0 <= nr < row and 0<= nc < col and visited[nr][nc] == 0 and grid[nr][nc] == '1':
-                    dfs(nr, nc)
-
+        directions = [(0,1), (1,0), (0, -1), (-1,0)]
 
         for i in range(row):
             for j in range(col):
                 if grid[i][j] == '1' and visited[i][j] == 0:
                     count += 1  # each connected component is an island
-                    dfs(i, j)
+
+                    q = deque([(i,j)])
+                    visited[i][j] = 1
+
+                    while q:
+                        r,c = q.popleft()
+                        for dr, dc in directions:
+                            nr, nc = r+dr, c+dc
+                            if 0<= nr < row and 0 <= nc < col and grid[nr][nc] == '1' and visited[nr][nc] == 0:
+                                visited[nr][nc] = 1
+                                q.append((nr,nc))
 
         return count
+
 
         
