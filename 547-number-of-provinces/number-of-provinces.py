499012f27faa40611c2ca1d407f0b95c70b1487d
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-19 14:17:36 +0530

    Time: 7 ms (56.23%) | Memory: 18.1 MB (99.99%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,25 +1,50 @@
 class Solution:
     def findCircleNum(self, provinces: List[List[int]]) -> int:
-        n = len(provinces)
-        visited = [0]*n
+        # union find way (DSU)
 
-        
-        count = 0 # number of provinces
-        for i in range(n): # one complete pass for disconnected components
+        n = len(provinces)  # number of nodes
+    
+        parent = [i for i in range(n)] # init parent of it is self
+
+        count = n # initially all the nodes are separate componests
+
+        # rank or size
+        rank = [1] * n  # initially their size will be 1 (think of it like subtree size)
+
+        def find(x): # iterative function to find root of x
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x  
+
+        def union(x,y):
+            rootx, rooty = find(x), find(y)
+
+            # no merge, if their roots are same (same components)
+            if rootx == rooty:
+                return 0
+
+            # merge operation
+            if rank[x] >= rank[y]:
+                parent[rooty] = rootx
+                rank[rootx] += rank[rooty]
             
-            if visited[i] == 0: # then only traverse
-                count += 1
-                q = deque([i])
-                visited[i] = 1
-                
-                while q:
-                    node = q.popleft()
-                    for nei in range(n):
-                        if provinces[node][nei] == 1 and visited[nei] == 0:
-                            visited[nei] = 1
-                            q.append(nei)
+            else:
+                parent[rootx] = rooty
+                rank[rooty] += rank[rootx]
+
+            return 1
+
+
+        for r in range(n):
+            for c in range(r+1, n): # only upper triangular is enough
+                if provinces[r][c]:
+                    count -= union(r,c)
 
         return count
+
+       
+        
 
 
         
