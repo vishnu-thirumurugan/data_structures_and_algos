a1965384a7e1878236240ea9571ead7ea2073220
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-20 07:41:37 +0530

    Time: 274 ms (24.00%) | Memory: 29.8 MB (5.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,66 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
 
-        # dfs way 
-        row, col = len(grid), len(grid[0])
+        if not grid:
+            return 0
 
-        visited = [[0]*col for _ in range(row)]
+        # union find way 
+        rows = len(grid)
+        cols = len(grid[0])
 
-        count = 0
+        # union declarations
+        parent = {}
+        rank = {} # subtree size 
 
-        directions = [(0,1), (1,0), (0, -1), (-1,0)]
+        def find(x):
+            if x != parent[x]:
+                parent[x] = find(parent[x])
 
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == '1' and visited[i][j] == 0:
-                    count += 1  # each connected component is an island
+            return parent[x]
 
-                    q = deque([(i,j)])
-                    visited[i][j] = 1
+        
+        def union(x, y):
+            root_x, root_y = find(x), find(y)
 
-                    while q:
-                        r,c = q.popleft()
-                        for dr, dc in directions:
-                            nr, nc = r+dr, c+dc
-                            if 0<= nr < row and 0 <= nc < col and grid[nr][nc] == '1' and visited[nr][nc] == 0:
-                                visited[nr][nc] = 1
-                                q.append((nr,nc))
+            if root_x == root_y: # already same set --> no merge req
+                return 0
+            
+            if rank[root_x] >= rank[root_y]:
+                parent[root_y] = root_x
+                rank[root_x] += root_y
+
+            else:
+                parent[root_x] = root_y
+                rank[root_y] += root_x
+
+            return 1
+
+        count = 0 # number of initial sets
+        # intialize parent and rank matrix and count
+        for r in range(rows):
+            for c in range(cols):
+                idx = r*cols + c
+                if grid[r][c] == '1':
+                    parent[idx] = idx
+                    rank[idx] = 1
+                    count += 1
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    idx = r * cols + c
+                    for dr, dc in [[1, 0],[0,1]]: # see only right and down --> reduces redundant calls
+                        nr, nc = r+dr, c+dc
+                        if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1':
+                            nidx = nr* cols + nc
+                            count -= union(idx, nidx)
 
         return count
+
+
+            
+
+
 
 
         
