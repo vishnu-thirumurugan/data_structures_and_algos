84336cef397c49252e1430b144e4fbc6d3ad7fae
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-30 14:46:26 +0530

    Time: 35 ms (11.88%) | Memory: 17.5 MB (88.90%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # 0/1 knpsack 
+
+        n = len(candidates)
+        res = []
+        candidates.sort() # sorting to avoid duplicates
+
+        def backtrack(start, path, curr_target):
+            if curr_target == 0:
+                res.append(path[:])
+                return
+
+            # n branches in the begining
+            for i in range(start, n):
+                # avoid duplicates at same level
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue # to next branch
+
+                if curr_target < 0:
+                    return
+
+                backtrack(i+1, path+[candidates[i]], curr_target - candidates[i])
+
+        backtrack(0,[],target)
+        return res
+
+
+        
+
+        
