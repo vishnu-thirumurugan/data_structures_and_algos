6ebffd7221cd34ff4b15d4a152cd5a68b309f027
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2025-12-31 08:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (89.44%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
+        def backtrack(idx, path):
+            if idx == n: # reached last idx
+                res.append(path[:])
+                return
+
+            # either u take and explore or leave and explore
+
+            # take and explore
+            path.append(nums[idx])
+            backtrack(idx+1, path)
+
+            # leave and explore
+            path.pop()
+            backtrack(idx+1, path)
+
+        res = []
+        backtrack(0,[])
+
+        return res
+            
+                
