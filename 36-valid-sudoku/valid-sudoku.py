8a9c380ae1bebfde00c808ae1f9af96cd6966de0
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-05 13:40:57 +0530

    Time: 4 ms (56.22%) | Memory: 17.5 MB (77.49%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,32 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # set to make the o(1) lookup
-        row = [[] for _ in range(9)]
-        col = [[] for _ in range(9)]
-        box = [[] for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
 
-        # initialize and fill
         for r in range(9):
             for c in range(9):
-                if board[r][c] != '.':
-                    num = board[r][c]
-                    row[r].append(num)
-                    col[c].append(num)
-                    box[3*(r//3)+ (c//3)].append(num)
+                num = board[r][c]
+                if num != '.':
+                    if num in rows[r] or num in cols[c] or num in boxes[3*(r//3) + (c//3)]:
+                        return False
 
-        for i in range(9):
-            counter_row = Counter(row[i])
-            counter_col = Counter(col[i])
-            counter_box = Counter(box[i])
-
-            if any(v > 1 for v in counter_row.values()):
-                return False
-
-            if any(v > 1 for v in counter_col.values()):
-                return False
-
-            if any(v > 1 for v in counter_box.values()):
-                return False
+                    else:
+                        rows[r].add(num)
+                        cols[c].add(num)
+                        boxes[3*(r//3) + (c//3)].add(num)
 
         return True
 
