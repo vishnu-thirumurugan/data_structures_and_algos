959ab17a93ebc04e79917b0c2ac09ad6f8b81852
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-07 07:32:41 +0530

    Time: 53 ms (86.93%) | Memory: 29.1 MB (6.82%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        # if there are even number of negatives, you can clash them
+        # if there is a zero, you can consider it as black hole and sink all negatives there
+        # it is handles implicitly, not explicitly
+
+        min_abs = math.inf
+        negative = 0
+        total_sum = 0
+
+        for row in matrix:
+            for val in row:
+                if val < 0:
+                    negative += 1
+                min_abs = min(min_abs, abs(val))  # this min val will be final negative
+                total_sum += abs(val)
+
+        return total_sum - 2*min_abs if negative % 2 == 1 else total_sum
+
+
+
+
+
+        
