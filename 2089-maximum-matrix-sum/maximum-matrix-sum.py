1d01b02d839fa4c5f0752e8ff973f1e04b528071
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-07 07:36:36 +0530

    Time: 75 ms (78.81%) | Memory: 29 MB (6.82%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -3,13 +3,14 @@ class Solution:
         # if there are even number of negatives, you can clash them
         # if there is a zero, you can consider it as black hole and sink all negatives there
         # it is handles implicitly, not explicitly
-
+        rows, cols = len(matrix), len(matrix[0])
         min_abs = float('inf')
         negative = 0
         total_sum = 0
 
-        for row in matrix:
-            for val in row:
+        for r in range(rows):
+            for c in range(cols):
+                val = matrix[r][c]
                 if val < 0:
                     negative += 1
                 min_abs = min(min_abs, abs(val))  # this min val will be final negative
