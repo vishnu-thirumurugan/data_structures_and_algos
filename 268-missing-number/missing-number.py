0ee738b1c936e31000ad0bb6287de15bd9f6a47d
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-10 18:51:09 +0530

    Time: 2 ms (64.75%) | Memory: 20.5 MB (5.07%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,14 +1,4 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # bit manipulation way 
         n = len(nums)
-        xor_all = 0
-
-        for i in range(n):
-            xor_all ^= i        # expected_array [0 to n]
-            xor_all ^= nums[i]  # original_array
-
-        # xor with n finally to complete expected array
-        xor_all ^= n
-
-        return xor_all
+        return int(n*(n+1)*0.5) - sum(nums)
