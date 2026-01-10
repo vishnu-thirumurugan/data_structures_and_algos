87de7f8f1dee6c450d3133806e313612b8c4e301
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-10 12:49:42 +0530

    Time: 5 ms (88.49%) | Memory: 19.5 MB (5.24%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,39 +1,32 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        # arrays to check previously filled queens
-        pure_left = [0]*n
-        left_upper_diagonal = [0]*(2*n-1)
-        left_lower_diagonal = [0]*(2*n-1)
+        # previous submission will be more intuitive
+        # this one is optimized
 
-        row = col = n
-        board = [['.']*n for _ in range(n)]
+        rows = [0]*n
+        diag1 = [0]*(2*n-1)
+        diag2 = [0]*(2*n-1)
+
         self.count = 0 
 
-        def backtrack(col,board,pure_left, left_lower_diagonal, left_upper_diagonal):
-            if col == n: # queens are placed in all cols
+        def backtrack(col):
+            if col == n:
                 self.count += 1
                 return
 
             for row in range(n):
-                if (board[row][col] == '.' 
-                and pure_left[row] == 0 
-                and left_lower_diagonal[row+col] == 0 
-                and left_upper_diagonal[n-1+col-row] == 0):
-                    pure_left[row] = 1
-                    left_lower_diagonal[row+col] = 1
-                    left_upper_diagonal[n-1+col-row] = 1
-                    board[row][col] = 'Q'
-                    # fill next column
-                    backtrack(col+1,board,pure_left, left_lower_diagonal, left_upper_diagonal)
-                    pure_left[row] = 0
-                    left_lower_diagonal[row+col] = 0
-                    left_upper_diagonal[n-1+col-row] = 0
-                    board[row][col] = '.'
+                if not rows[row] and not diag1[row+col] and not diag2[n-1+col-row]:
+                    rows[row] = 1
+                    diag1[row+col] = 1
+                    diag2[n-1+col-row] = 1
+                    backtrack(col+1)
+                    rows[row] = 0
+                    diag1[row+col] = 0
+                    diag2[n-1+col-row] = 0
+            
+        backtrack(0)
 
-
-        backtrack(0,board,pure_left, left_lower_diagonal, left_upper_diagonal)
         return self.count
-
 
 
 
