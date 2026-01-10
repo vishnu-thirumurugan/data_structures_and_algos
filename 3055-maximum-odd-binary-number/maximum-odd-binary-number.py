0be8ad09fcfd7043bc994b5bcf18572585d2029f
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-10 18:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (11.25%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,22 +1,8 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        count = 0
+        # clean and optimized way is to take advantage of string
+        ones = s.count('1')
         n = len(s)
-        num = int(s, 2)
-        while num:
-            if num & 1:
-                count += 1
-            num //= 2 # u can also use right shift operator
-        
-        res = ''
-        # keep one '1' at the end and others at the begining
-        # put the zeros in between
-        for _ in range(count-1):
-            res += '1'
-        for _ in range(n-count):
-            res += '0'
-        
-        res += '1'
 
-        return res
+        return '1'*(ones-1) + '0'*(n-ones)+ '1'
         
