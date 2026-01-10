c5c0eb9a9c9ce7aaed88e1a6a3cdcc4812d2bc5b
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-10 13:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.90%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -2,39 +2,25 @@ class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         matrix = [[0]*n for _ in range(n)]
 
-        num = 1 # start from 1 and fill
+        dirs = [(0,1), (1,0), (0, -1), (-1, 0)] # R--> D--> L-->U
+        d = 0
+        r = c = 0
 
-        # top --> left to right 
-        # bottom --> right to left 
-        # left --> bottom to top
-        # right --> top to bottom
+        for num in range(1, n*n+1):
+            matrix[r][c] = num
 
-        top, bottom = 0, n-1
-        left, right = 0, n-1
+            nr, nc = r + dirs[d][0], c + dirs[d][1]
 
-        while top <= bottom and left <= right:
-            # left --> right
-            for col in range(left,right+1):
-                matrix[top][col] = num
-                num += 1
-            top += 1 # one top row filled
+            # change directions as per constraints
+            if not (0<=nr<n and  0<=nc<n and matrix[nr][nc] == 0):
+                d = (d+1)%4
+                nr, nc = r + dirs[d][0], c + dirs[d][1]
 
-            # top --> bottom
-            for row in range(top, bottom+1):
-                matrix[row][right] = num
-                num += 1
-            right -= 1
-
-            for col in range(right, left-1, -1):
-                matrix[bottom][col] = num
-                num += 1
-            bottom -= 1
-
-            for row in range(bottom, top-1, -1):
-                matrix[row][left] = num
-                num += 1
-            left += 1
+            r, c = nr, nc
 
         return matrix
+
+
+
 
         
