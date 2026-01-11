192a78d4e77acc913e1c63ed4050dac0e4b6c72a
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-11 15:43:57 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.77%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,14 +1,11 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        # bit manipulation way
-        n = len(nums) 
-        summ = 0
-        # mask runs from 0 to pow (2, n) -1 
-        for mask in range((1<<n)): #--> generates all the subset
-            xor_total = 0
-            # for each bit in the mask
-            for i in range(n):
-                if mask & (1<<i):            #(1<<i) checks i th bit is set or not
-                    xor_total ^= nums[i]
-            summ += xor_total
-        return summ
+        # bit manipulation with o(n) --> aryan mittal channel
+        OR = 0
+        n = len(nums)
+        for num in nums:
+            OR |= num
+        # each number appears in half of the subsets
+        # there are 2**n number of subsets
+        # OR sets bit if atleast one of the bits is set
+        return OR * (1<<(n -1))
