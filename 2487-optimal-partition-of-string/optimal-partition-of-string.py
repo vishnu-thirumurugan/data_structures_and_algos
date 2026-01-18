481dc3232de493443c2ceaa13278b031815d21b3
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-18 15:43:15 +0530

    Time: 75 ms (20.58%) | Memory: 19.7 MB (23.88%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -1,22 +1,18 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         # bit manipulation > array > set for same o(n)
-        n = len(s)
         flag = 0
         count = 1 # string length constraint --> atleast one string will be present
 
-        i = 0  # index to move on string
-        while i < n:
-            val = ord(s[i])-ord('a')
-            # print(val)
+
+        for i in s:
+            val = ord(i)-ord('a')
             if flag & (1<<val): # the element is already present
                 count += 1   # create new sub-string
                 flag = 0   # reset flag for next cycle
             
             flag = flag | (1<<val) # if not present keep track / first element of sub-string
-            # print(flag)
-            i+= 1
-
+    
         return count
 
 
