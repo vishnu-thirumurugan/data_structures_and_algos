eae4620e86e3c58c813bed167da4fa7096f2f279
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-22 06:00:03 +0530

    Time: 25 ms (34.97%) | Memory: 20.6 MB (17.52%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -4,9 +4,8 @@ class Solution:
         for i in range(32): # 32 bit integer
             ibit_sum = 0 
             for num in nums:
-                if (num >> i) & 1:
-                    ibit_sum += 1
-                    ibit_sum %= 3 # --> you get zero or 1 
+                ibit_sum += (num >> i) & 1
+            ibit_sum %= 3 # --> you get zero or 1 
             if ibit_sum != 0:
                 ans |= (1 << i)
 
