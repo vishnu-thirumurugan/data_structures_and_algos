5a9a7c92e28a9386353936a04221370ce030fbcf
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-22 05:58:42 +0530

    Time: 27 ms (33.60%) | Memory: 20.9 MB (10.70%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -8,7 +8,7 @@ class Solution:
                     ibit_sum += 1
                     ibit_sum %= 3 # --> you get zero or 1 
             if ibit_sum != 0:
-                ans |= (ibit_sum << i)
+                ans |= (1 << i)
 
         if ans >= (1 << 31):
             ans -= (1 << 32)
