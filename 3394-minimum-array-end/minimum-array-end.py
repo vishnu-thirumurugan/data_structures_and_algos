b9f93418ef24095025ab4cc0591b4a93700dc3ef
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-01-25 22:38:05 +0530

    Time: 3 ms (29.13%) | Memory: 19.3 MB (19.90%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.py
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -1,33 +1,13 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        # create such an array and see you will get an idea
-        bin_form = bin(n-1)[2:]
-        # print(bin_form)
-    
-        # fill these values of zeros at x from right to left
-        res = []
-        while x:
-            if x & 1 == 0:
-                if bin_form:
-                    res.append(bin_form[-1])
-                    bin_form = bin_form[:-1]
-                else:
-                    res.append('0')
+        k = n - 1
+        bit = 0
 
-            else:
-                res.append('1')
-            
-            x >>= 1
+        while k:
+            if (x & (1 << bit)) == 0:
+                if k & 1:
+                    x |= (1 << bit)
+                k >>= 1
+            bit += 1
 
-        
-        return int(bin_form + ''.join(reversed(res)),2)
-
-        
-
-
-
-
-
-
-
-        
+        return x
