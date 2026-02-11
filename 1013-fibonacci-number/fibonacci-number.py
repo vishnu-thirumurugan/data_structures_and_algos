bfc244d85c0569425ea02b54dcc7caa8a2b71318
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-02-11 09:15:09 +0530

    Time: 47 ms (52.52%) | Memory: 19.3 MB (37.99%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,13 +2,14 @@ class Solution:
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-            
-        table = [0]*(n+1)
-        table[0] = 0
-        table[1] = 1
 
-        for i in range(2,len(table)):
-            table[i] = table[i-1] + table[i-2]
-        
-        return table[-1]
+        prev = 0
+        curr = 1
+
+        for i in range(n):
+            temp = curr
+            curr = curr + prev
+            prev = temp
+
+        return prev
         
