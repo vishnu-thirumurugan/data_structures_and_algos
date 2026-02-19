b185171bc992147032a34605eae4691897e6abdc
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-02-19 19:51:05 +0530

    Time: 173 ms (67.30%) | Memory: 32.5 MB (8.93%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -3,12 +3,15 @@ class Solution:
         left = 0 
         maxSum = 0
         currSum = 0
+        seen = set()
 
         for right in range(len(nums)):
-            while nums[right] in nums[left:right]:
+            while nums[right] in seen:
                 currSum -= nums[left]
+                seen.remove(nums[left])
                 left += 1
 
+            seen.add(nums[right])
             currSum += nums[right]
             maxSum = max(maxSum, currSum)
 
