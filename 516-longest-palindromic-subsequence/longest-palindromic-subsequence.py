014a567625cfdf1131b984959599ec7451814601
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-02-26 09:05:12 +0530

    Time: 1306 ms (22.44%) | Memory: 44.7 MB (32.60%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -2,35 +2,17 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         t = s[::-1]
         n = len(s)
-        memo = {}
 
-        def dp(i,j):
-            if j >= n or i >= n:
-                return 0
+        dp = [[0]*(n+1) for _ in range(n+1)]
 
-            if (i,j) in memo:
-                return memo[(i,j)]
-            
-            if s[i] == t[j]:
-                memo[(i,j)] = 1 + dp(i+1, j+1)
-            else:
-                memo[(i,j)] = max(dp(i+1,j), dp(i,j+1))
-
-            return memo[(i,j)]
-
-        return dp(0,0)
-
-
-        dp = [[0*n] for _ in range(n)]
-
-        for i in range(1, n):
-            for j in range(1, n):
-                if s[i] == t[j]:
-                    dp[i][j] = 2 + dp[i-1][j-1]
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
 
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
-        return dp[n-1][n-1]
+        return dp[n][n]
 
 
