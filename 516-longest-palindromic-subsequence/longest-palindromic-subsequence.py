3b1eef9dfe1079b88d08302fbae0f0cfc4abc1b3
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-02-26 08:56:16 +0530

    Time: 1848 ms (5.00%) | Memory: 547.4 MB (5.39%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -21,3 +21,16 @@ class Solution:
         return dp(0,0)
 
 
+        dp = [[0*n] for _ in range(n)]
+
+        for i in range(1, n):
+            for j in range(1, n):
+                if s[i] == t[j]:
+                    dp[i][j] = 2 + dp[i-1][j-1]
+
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[n-1][n-1]
+
+
