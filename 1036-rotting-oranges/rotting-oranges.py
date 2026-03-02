95a6fbe939f5bca9361356a9e5f7b2d5754c5cb0
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-03 05:18:50 +0530

    Time: 3 ms (76.79%) | Memory: 19.3 MB (73.95%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,34 +1,39 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        # not reachability
-        # this is propogation over time
-        rows, cols = len(grid), len(grid[0])
-        q = deque([])
-        fresh = 0
-        # multi source bfs --> identify the sources 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
-                    q.append((r,c))
+        rows = len(grid)
+        cols = len(grid[0])
 
-        # bfs 
+        fresh = 0
+        q = deque([])
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    q.append((i,j))
+        print(fresh)
         level = 0
         while q and fresh > 0:
-            size = len(q) #   level wise
+            size = len(q)
             for _ in range(size):
                 r, c = q.popleft()
-                for dr, dc in ((0,1), (1,0), (-1,0), (0,-1)):
+                for dr, dc in ([0,1], [1,0],[-1,0],[0,-1]):
                     nr, nc = r+dr, c+dc
-                    if 0<= nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
-                        fresh -= 1
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
-                        q.append((nr, nc))
+                        fresh -= 1
+                        q.append((nr,nc))
 
             level += 1
-
+        print(fresh)
         return level if fresh == 0 else -1
+
+
+
+
+
+
 
                 
 
