fe8048a8b7a7b5f68eb88d356ae20f59d8aade06
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-05 10:07:56 +0530

    Time: 662 ms (34.53%) | Memory: 22.1 MB (88.23%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,37 +1,40 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort() # to remove duplicates from index i 
+        n = len(nums)
         result = []
 
-        n = len(nums)
-
-        
         for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
+            
+            left = i + 1
+            right = n - 1
 
-            left = i+1
-            right = n-1
-
-            while  left < right:
+            while left < right:
                 s = nums[i] + nums[left] + nums[right]
                 if s == 0:
                     result.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-
-                    while left <  right and nums[left-1] == nums[left]:
+                    
+                    # avoid duplicates coming from the left
+                    while left < right and nums[left] == nums[left-1]:
                         left += 1
+                    # avoid duplicates coming from the right
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
 
-                    while left <  right and nums[right] == nums[right+1]:
-                        right -=1 
-
-                elif s < 0:
-                    left += 1
-                else:
+                # if sum > 0, reduce right
+                elif s > 0:
                     right -= 1
-
+                # if sum < 0, need more for contribution --> increase left 
+                else:
+                    left += 1
+                
         return result
+
+            
 
             
 
