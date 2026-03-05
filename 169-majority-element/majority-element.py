f8127e56f97ae1916f4f920236b573ab41823dc7
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-05 12:54:24 +0530

    Time: 7 ms (57.72%) | Memory: 21.2 MB (45.38%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # boyer moore majority vote 
-        # o(n) and o(1)
-        count = 0
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-
-            count += 1 if candidate == num else -1
-
-        return candidate
+        # not O(1)
+        freq = Counter(nums)
+        return max(freq, key = freq.get)
         
