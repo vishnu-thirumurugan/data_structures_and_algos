94e20dfd48364f69ecf7ac9322238e7626b27ca1
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-05 12:57:19 +0530

    Time: 3 ms (86.15%) | Memory: 21.2 MB (80.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # not O(1)
-        freq = Counter(nums)
-        return max(freq, key = freq.get)
-        
+        count = 0 
+        for num in nums:
+            if count == 0:
+                candidate = num
+
+            count += 1 if candidate == num else -1
+
+        return candidate
