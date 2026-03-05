71cb1df57ecb06342b939b435d2e8c2790a61fbb
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-06 04:57:38 +0530

    Time: 443 ms (64.97%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,4 @@
-select w2.id from Weather w1, Weather w2
-where datediff(w2.recordDate,w1.recordDate) = 1
+select w2.id from 
+Weather w1 join Weather w2
+on datediff(w2.recordDate,w1.recordDate) = 1
 and w2.temperature > w1.temperature
