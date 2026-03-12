e639ace268b94e30110f22068f0363229b18d0eb
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-12 10:44:48 +0530

    Time: 4 ms (68.00%) | Memory: 22.9 MB (77.47%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,11 @@
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # intuition - break the loop after k 
+        # most_common() function way
         c = Counter(nums)
-        sorted_list = sorted(c.items(), key = lambda x:x[1], reverse = True)
+        top_k = c.most_common(k)
         res = []
-        for key, val in sorted_list:
-            if k == 0:
-                break
-            k-=1
+        for key,_ in top_k:
             res.append(key)
         return res
 
