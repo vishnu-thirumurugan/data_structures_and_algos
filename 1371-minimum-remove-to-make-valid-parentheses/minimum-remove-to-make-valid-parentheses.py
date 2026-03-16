a56c157dae97f0a4dd527922b7c2f098b0ee3875
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-16 09:29:05 +0530

    Time: 23 ms (97.77%) | Memory: 20.9 MB (50.93%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,32 +1,33 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        open_count = 0 
+        open_count = 0
         res = []
 
-        # first loop removes extra right bracket  --> ')'
-        for i in range(len(s)):
-            if s[i] == '(':
+        for i, ch in enumerate(s):
+            if ch == '(':
                 open_count += 1
-                res.append(s[i])
-            elif s[i] == ')':
+                res.append(ch)
+
+            elif ch == ')':
                 if open_count > 0:
                     open_count -= 1
-                    res.append(')')
-                # else ignore this extra right bracket
+                    res.append(ch)
+                else:
+                    pass
 
-            else: # if its a character --> append it 
-                res.append(s[i])
-
-        # second loop to avoid extra left bracket:
-        n = len(res)
-        final_res = []
-        for i in range(n-1, -1, -1):
-            if  open_count > 0 and res[i] == '(':
-                open_count -= 1
             else:
-                final_res.append(res[i])
+                res.append(ch)
 
-        return ''.join(reversed(final_res))
+        
+        for i in range(len(res)-1,-1,-1):
+            if open_count == 0:
+                break
+            if res[i] == '(' and open_count > 0:
+                del res[i]
+                open_count -= 1
+
+        return ''.join(res)
+
 
 
 
