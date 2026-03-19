9921dec0df8ab0fd071b4c3b23d6c4e136329de1
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-19 12:03:56 +0530

    Time: 15 ms (22.68%) | Memory: 19.5 MB (89.56%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,20 +2,20 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         n = len(candidates)
-        # unbounded pick or not pick
+
         def backtrack(idx, path, curr_target):
             if idx == n and curr_target == 0:
                 res.append(path.copy())
+                return
 
-            if curr_target < 0 or idx >= n:
+            if idx >= n or curr_target < 0:
                 return
 
             # pick
             path.append(candidates[idx])
-            backtrack(idx, path, curr_target - candidates[idx]) # unbounded --> idx stays
-            # not pick
+            backtrack(idx,path, curr_target-candidates[idx]) # you can use the same element again
             path.remove(candidates[idx])
-            backtrack(idx + 1, path, curr_target)
+            backtrack(idx+1, path, curr_target)
 
         backtrack(0,[],target)
         return res
