8243fab52e8d997c99ba13b69c76373595a69c4e
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-03-21 23:44:35 +0530

    Time: 105 ms (42.93%) | Memory: 28.1 MB (86.59%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -6,11 +6,11 @@ class Solution:
         for i in range(len(nums)-1,-1,-1):
             while stack and nums[stack[-1]] <= nums[i]:
                 stack.pop()
-            
-            res.append(stack[-1]-i if stack else 0)
-            stack.append(i)
-        return res[::-1]
 
+            res.append(stack[-1] - i if stack else 0)
+            stack.append(i)
+
+        return res[::-1]
 
 
             
