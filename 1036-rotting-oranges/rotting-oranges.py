8fef692e9c50c2d84ece89bcd8cd9e0d91271dc3
Vishnu Thirumurugan <99240842+vishnu-thirumurugan@users.noreply.github.com>
2026-04-10 08:55:09 +0530

    Time: 4 ms (35.53%) | Memory: 19.2 MB (84.44%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,31 +3,31 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-        fresh = 0
         q = deque([])
-
+        fresh = 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
-                    fresh += 1
-                elif grid[i][j] == 2:
+                if grid[i][j] == 2:
                     q.append((i,j))
-        print(fresh)
+                elif grid[i][j] == 1:
+                    fresh += 1
+
         level = 0
         while q and fresh > 0:
             size = len(q)
             for _ in range(size):
-                r, c = q.popleft()
-                for dr, dc in ([0,1], [1,0],[-1,0],[0,-1]):
+                r,c = q.popleft()
+                for dr, dc in [(0,1), (1,0), (-1,0), (0, -1)]:
                     nr, nc = r+dr, c+dc
-                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
+                    if 0<= nr< rows and 0<=nc< cols and grid[nr][nc] == 1:
+                        q.append((nr,nc))
                         grid[nr][nc] = 2
                         fresh -= 1
-                        q.append((nr,nc))
 
             level += 1
-        print(fresh)
+
         return level if fresh == 0 else -1
+
 
 
 
